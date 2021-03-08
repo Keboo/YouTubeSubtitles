d7d7ab161a6d21e87d5e09068a542922fd31188a
Kevin Bost <kitokeboo@gmail.com>
2021-03-07 23:34:13 -0800

    Finally working

--- SubtitleConverter/StreamingTools/YouTube/YouTubeBrowser.cs
+++ SubtitleConverter/StreamingTools/YouTube/YouTubeBrowser.cs
@@ -1,16 +1,14 @@
-﻿using PInvoke;
+﻿using FlaUI.Core.Input;
+using FlaUI.Core.WindowsAPI;
 using PlaywrightSharp;
 using System;
-using System.Buffers.Text;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Net.NetworkInformation;
-using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
-using static PInvoke.User32;
+using User32 = PInvoke.User32;
 
 namespace StreamingTools.YouTube
 {
@@ -27,25 +25,35 @@ namespace StreamingTools.YouTube
             RecoveryEmail = recoveryEmail;
         }
 
-        private delegate bool EnumWindowProc(IntPtr hwnd, IntPtr lParam);
-
-        public async Task UploadAsync()
+        public async Task UploadAsync(
+            string filePath, 
+            string title, 
+            string description, 
+            IReadOnlyCollection<string> playlists, 
+            IReadOnlyCollection<string> tags)
         {
-            string filePath = @"D:\OneDrive\Videos\Jubilee Nutcracker 2019_1.mp4";
+            //string filePath = @"C:\Users\kitok\OneDrive\Videos\Jubilee Nutcracker 2019_1.mp4";
+            //string title = "Kevin's Test Title Video";
+            //string description = "This is a test description to make sure it works";
+            //IReadOnlyList<string> playlists = new List<string>
+            //{
+            //    "C# Programming"
+            //};
+            //IReadOnlyList<string> tags = new List<string>
+            //{
+            //    "Tag 1",
+            //    "Programming"
+            //};
+            //string filePath = @"D:\OneDrive\Videos\Jubilee Nutcracker 2019_1.mp4";
             //string filePath = @"D:\OneDrive\Pictures\Camera Roll\VID_20210220_134134.mp4";
-            //var base64Encode = Base64Encode(filePath);
-            //await base64Encode;
             using var playwright = await Playwright.CreateAsync();
 
             await using var browser = await playwright.Chromium.LaunchAsync(headless: false);
             
             var page = await browser.NewPageAsync();
             await page.GoToAsync("https://studio.youtube.com/");
-            //IElementHandle? signIn = await FirstWithText(page, "a > paper-button yt-formatted-string", "Sign In");
-            //await signIn.ClickAsync();
             await page.TypeAsync("input[type=\"email\"]", Username);
             await page.ClickAsync(":text('Next')");
-            //await Task.Delay(TimeSpan.FromSeconds(1));
             await page.WaitForSelectorAsync("input[type=\"password\"]");
             await page.TypeAsync("input[type=\"password\"]", Password);
             var navTask = page.WaitForNavigationAsync();
@@ -66,129 +74,86 @@ namespace StreamingTools.YouTube
                 await Task.Delay(100);
             }
 
-
             await page.ClickAsync("#avatar-btn");
             await page.ClickAsync(":text('Switch account')");
             await page.ClickAsync(":text('Kevin Bost')");
             await page.ClickAsync("ytcp-icon-button[aria-label=\"Upload videos\"]");
-
-            var tcs = new TaskCompletionSource<int>();
-            //page.FileChooser += Page_FileChooser;
             
             await page.ClickAsync("#select-files-button");
 
-            string title = await page.GetTitleAsync();
-            await Task.Delay(3000);
-            var chomeProcesses = Process.GetProcessesByName("chrome").Where(x => !string.IsNullOrWhiteSpace(x.MainWindowTitle)).ToList();
-
-            var lParam = new IntPtr(0x0000_0000);
-            foreach (var process in chomeProcesses)
+            string windowTitle = await page.GetTitleAsync();
+            
+            var browserProcess = Process.GetProcesses()
+                .Where(x => x.MainWindowTitle.Contains(windowTitle)).ToList();
+            await Task.Delay(2000);
+            foreach (var process in browserProcess)
             {
-                var callback = new WNDENUMPROC((x, y) =>
-                {
+                var callback = new User32.WNDENUMPROC((x, y) =>
+                {                    
                     if (process.MainWindowHandle == User32.GetParent(x) &&
                         User32.GetWindowText(x) == "Open")
                     {
-                        User32.SendMessage(x, User32.WindowMessage.WM_CHAR, new IntPtr('h'), lParam);
+                        Keyboard.Type(filePath);
+                        Keyboard.Press(VirtualKeyShort.ENTER);
                         return false;
                     }
                     return true;
                 });
                 User32.EnumWindows(callback, IntPtr.Zero);
-
-                //User32.SendMessage(process.MainWindowHandle, User32.WindowMessage.WM_CHAR, new IntPtr('K'), lParam);
-                //
-                //int ct = 0;
-                //IntPtr prevChild = IntPtr.Zero;
-                //IntPtr currChild = IntPtr.Zero;
-                //while (true && ct < 10)
-                //{
-                //    currChild = PInvoke.User32.FindWindowEx(process.MainWindowHandle, prevChild, "#32770", null);
-                //    if (currChild == IntPtr.Zero) break;
-                //    //var info = new PInvoke.User32.WINDOWINFO();
-                //    string childTitle = PInvoke.User32.GetWindowText(currChild);
-                //    User32.SendMessage(currChild, User32.WindowMessage.WM_CHAR, new IntPtr('h'), lParam);
-                //
-                //    //if (PInvoke.User32.GetWindowInfo(currChild, ref info))
-                //    //{
-                //    //
-                //    //}
-                //    //result.Add(currChild);
-                //    prevChild = currChild;
-                //    ++ct;
-                //}
             }
 
             await Task.Delay(300);
-            await page.Keyboard.TypeAsync("test");
-            //await tcs.Task;
 
             //TODO: Set metadata
+            var titleRequired = await page.WaitForSelectorAsync(":text('Title (required)')");
+            await Task.Delay(500);
+            await page.TypeAsync("#textbox[aria-label=\"Add a title that describes your video\"]", title);
+            await page.TypeAsync("#textbox[aria-label=\"Tell viewers about your video\"]", description);
+            await page.ClickAsync("span.ytcp-text-dropdown-trigger:has-text('Select')");
+            foreach(var playlist in playlists)
+            {
+                await page.ClickAsync($"ytcp-ve li.ytcp-checkbox-group:has-text('{playlist}')");
+            }
+            await page.ClickAsync("ytcp-button.done-button[label=\"Done\"]");
+            await page.ClickAsync("div.ytcp-button:has-text('More options')");
+            await page.TypeAsync("input[aria-label=\"Tags\"]", string.Join(',', tags));
+
+            await page.ClickAsync("ytcp-text-dropdown-trigger[label=\"Recording date\"]");
+            await page.PressAsync("paper-input[aria-label=\"Enter date\"] >> input", "Control+a");
+            await page.TypeAsync("paper-input[aria-label=\"Enter date\"]", $"{DateTime.Now:MM/dd/yyyy}");
+            await page.PressAsync("paper-input[aria-label=\"Enter date\"] >> input", "Escape");
+
+            await page.ClickAsync("#location >> input");
+            await page.TypeAsync("#location >> input", "Spokane WA");
+            await page.ClickAsync("paper-item:has-text('Spokane WA')");
 
             await page.ClickAsync("#next-button");
 
-            //TODO: Set cards
+            //Set cards, can't be done until after the video is done processing
             await page.ClickAsync("#next-button");
 
             //Make private
             await page.ClickAsync("paper-radio-button[name=\"PRIVATE\"]");
 
-            //for (string progress = await page.GetTextContentAsync(@"span.progress-label");
-            //    (progress ?? "").Contains("processing", StringComparison.OrdinalIgnoreCase);
-            //    progress = await page.GetTextContentAsync(@"span.progress-label"))
-            //{
-            //    await Task.Delay(100);
-            //}
             await WaitFor(() => page.IsEnabledAsync("#done-button"));
-            await page.ClickAsync("#done-button");
-
-            //IElementHandle? upload = await FirstWithText(page, "#endpoint", "Upload video");
-            //await upload.ClickAsync();
-
-            await Task.Delay(TimeSpan.FromSeconds(10));
-
-            async void Page_FileChooser(object? sender, FileChooserEventArgs e)
+            //Wait for upload complete
+            await WaitFor(async () => 
             {
-                page.FileChooser -= Page_FileChooser;
-                
-                //await e.SetFilesAsync(new FilePayload
-                //{
-                //    Name = Path.GetFileName(filePath),
-                //    MimeType = GetMimeType(filePath),
-                //    //Buffer = await base64Encode
-                //});
-                tcs.TrySetResult(0);
-            }
-        }
-
-        private static bool TestCallbase(IntPtr hwnd, IntPtr lParam)
-        {
-            return true;
-        }
-
-        private static async Task<string> Base64Encode(string filePath)
-        {
-            using FileStream inputFile = new(filePath, FileMode.Open, FileAccess.Read, FileShare.Read, bufferSize: 1024 * 1024, useAsync: false);
-            using CryptoStream base64Stream = new(inputFile, new ToBase64Transform(), CryptoStreamMode.Read);
+                string statusText = await page.GetInnerTextAsync("span.ytcp-video-upload-progress");
+                return !statusText.Contains("Uploading");
+            }, TimeSpan.FromHours(2));
             
-            using var sr = new StreamReader(base64Stream, bufferSize: 1024 * 1024);
-            return await sr.ReadToEndAsync();
+            await page.ClickAsync("#done-button");
         }
 
-        private static async Task WaitFor(Func<Task<bool>> condition)
+        private static async Task WaitFor(Func<Task<bool>> condition, TimeSpan? timeout = null)
         {
+            timeout ??= TimeSpan.FromSeconds(30);
             var sw = Stopwatch.StartNew();
             bool state;
-            while (!(state = await condition()) && sw.Elapsed < TimeSpan.FromSeconds(30))
+            while (!(state = await condition()) && sw.Elapsed < timeout)
             { }
             if (!state) throw new Exception("Failed timeout while waiting for condition");
-        }
-
-        private static string GetMimeType(string filePath)
-        {
-            return typeof(PlaywrightSharp.Helpers.StringExtensions)
-                .GetMethod("MimeType", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static)?
-                .Invoke(null, new object?[] { filePath }) as string ?? "";
         }
     }
 }
--- SubtitleConverter/VideoConverter/Program.cs
+++ SubtitleConverter/VideoConverter/Program.cs
@@ -43,86 +43,83 @@ namespace VideoConverter
             twitchClientId ??= section["TwitchClientId"] ?? throw new InvalidOperationException("No Twitch client id specified");
             twitchClientSecret ??= section["TwitchClientSecret"] ?? throw new InvalidOperationException("No Twitch client secret specified");
 
-            var youtubeSection = config.GetSection("YouTube");
+            var storageAccount = StorageAccount.Get(azureStorageAccountKey, config);
+            var tableClient = storageAccount.CreateCloudTableClient();
+            var streamVideoTables = tableClient.GetTableReference("streamvideos");
+            var youtubeSettingsTable = tableClient.GetTableReference("youtubesettings");
 
-            YouTubeBrowser browser = new(youtubeSection["Username"], youtubeSection["Password"], youtubeSection["RecoveryEmail"]);
-            await browser.UploadAsync();
-            //var storageAccount = StorageAccount.Get(azureStorageAccountKey, config);
-            //var tableClient = storageAccount.CreateCloudTableClient();
-            //var streamVideoTables = tableClient.GetTableReference("streamvideos");
-            //var youtubeSettingsTable = tableClient.GetTableReference("youtubesettings");
-            //
-            //TwitchAPI api = new(settings: new ApiSettings()
-            //{
-            //    ClientId = twitchClientId,
-            //    Secret = twitchClientSecret
-            //});
-            //
-            //YouTubeService youTubeService = await YouTubeFactory.GetServiceAsync(
-            //    new CloudTableDataStore(youtubeSettingsTable),
-            //    config,
-            //    youTubeClientId,
-            //    youTubeClientSecret,
-            //    YouTubeService.Scope.YoutubeUpload);
-            //
-            //var httpClient = new HttpClient();
-            //var twitchClinet = new Twitch(httpClient);
-            //
-            ////return;
-            //var videoResponse = await api.Helix.Videos.GetVideoAsync(userId: twitchUserId);
-            //
-            //foreach (TwitchVideo video in videoResponse.Videos)
-            //{
-            //    // Check if video exists in storage
-            //    VideoRow? row = streamVideoTables.CreateQuery<VideoRow>()
-            //        .Where(x => x.PartitionKey == nameof(VideoConverter) && x.TwitchVideoId == video.Id)
-            //        .FirstOrDefault();
-            //    if (!string.IsNullOrWhiteSpace(row?.YouTubeVideoId))
-            //    {
-            //        console.Out.WriteLine($"Twitch video {video.Id} already has YouTube id '{row.YouTubeVideoId}'; skipping");
-            //        continue;
-            //    }
-            //    console.Out.WriteLine($"Processing Twitch video {video.Id}");
-            //
-            //    string downloadedFilePath = await twitchClinet.DownloadVideoFileAsync(video.Id);
-            //    console.Out.WriteLine($"Downloaded video to '{downloadedFilePath}'");
-            //
-            //    string trimmedFilePath = await Ffmpeg.TrimLeadingSilence(downloadedFilePath);
-            //    if (string.IsNullOrWhiteSpace(trimmedFilePath))
-            //    {
-            //        console.Error.WriteLine($"Failed to trim silence from '{downloadedFilePath}'");
-            //        return 1;
-            //    }
-            //    console.Out.WriteLine($"Trimmed silence '{trimmedFilePath}'");
-            //    File.Delete(downloadedFilePath);
-            //
-            //    string youTubeId = await UploadVideoAsync(youTubeService, trimmedFilePath, video, console);
-            //    if (string.IsNullOrWhiteSpace(youTubeId))
-            //    {
-            //        console.Error.WriteLine($"Failed to upload '{trimmedFilePath}'");
-            //        return 1;
-            //    }
-            //    console.Out.WriteLine($"Uploaded to YouTube '{youTubeId}'");
-            //
-            //    row!.TwitchVideoId = video.Id;
-            //    row.TwitchPublishedAt = DateTime.Parse(video.PublishedAt ?? video.CreatedAt);
-            //    row.YouTubeVideoId = youTubeId;
-            //    TableOperation insertOperation = TableOperation.InsertOrMerge(row);
-            //
-            //    // Execute the operation.
-            //    TableResult _ = await streamVideoTables.ExecuteAsync(insertOperation);
-            //
-            //    break;
-            //}
+            TwitchAPI api = new(settings: new ApiSettings()
+            {
+                ClientId = twitchClientId,
+                Secret = twitchClientSecret
+            });
+
+            var httpClient = new HttpClient();
+            var twitchClinet = new Twitch(httpClient);
+
+            //return;
+            var videoResponse = await api.Helix.Videos.GetVideoAsync(userId: twitchUserId);
+
+            foreach (TwitchVideo video in videoResponse.Videos)
+            {
+                // Check if video exists in storage
+                VideoRow? row = streamVideoTables.CreateQuery<VideoRow>()
+                    .Where(x => x.PartitionKey == nameof(VideoConverter) && x.TwitchVideoId == video.Id)
+                    .FirstOrDefault();
+                if (!string.IsNullOrWhiteSpace(row?.YouTubeVideoId))
+                {
+                    console.Out.WriteLine($"Twitch video {video.Id} already has YouTube id '{row.YouTubeVideoId}'; skipping");
+                    continue;
+                }
+                console.Out.WriteLine($"Processing Twitch video {video.Id}");
+
+                string downloadedFilePath = @"C:\Users\kitok\AppData\Local\Temp\tmpA6A.mp4";
+                //string downloadedFilePath = await twitchClinet.DownloadVideoFileAsync(video.Id);
+                console.Out.WriteLine($"Downloaded video to '{downloadedFilePath}'");
+
+                string trimmedFilePath = await Ffmpeg.TrimLeadingSilence(downloadedFilePath);
+                await DeleteFile(downloadedFilePath);
+                if (string.IsNullOrWhiteSpace(trimmedFilePath))
+                {
+                    console.Error.WriteLine($"Failed to trim silence from '{downloadedFilePath}'");
+                    return 1;
+                }
+                console.Out.WriteLine($"Trimmed silence '{trimmedFilePath}'");
+
+                string youTubeId = await UploadVideoAsync(config, trimmedFilePath, video);
+                await DeleteFile(trimmedFilePath);
+                if (string.IsNullOrWhiteSpace(youTubeId))
+                {
+                    console.Error.WriteLine($"Failed to upload '{trimmedFilePath}'");
+                    return 1;
+                }
+                console.Out.WriteLine($"Uploaded to YouTube '{youTubeId}'");
+
+                row!.TwitchVideoId = video.Id;
+                row.TwitchPublishedAt = DateTime.Parse(video.PublishedAt ?? video.CreatedAt);
+                row.YouTubeVideoId = youTubeId;
+                TableOperation insertOperation = TableOperation.InsertOrMerge(row);
+
+                // Execute the operation.
+                TableResult _ = await streamVideoTables.ExecuteAsync(insertOperation);
+
+                break;
+            }
             return 0;
         }
 
-        private static async Task<string> UploadVideoAsync(YouTubeService service, 
-            string videoPath, TwitchVideo video, IConsole console)
+        private static async Task<string> UploadVideoAsync(
+            IConfiguration config,
+            string videoPath,
+            TwitchVideo video)
         {
             string description = video.Description;
 
-            List<string> tags = new()
+            HashSet<string> playlists = new()
+            {
+                "C# Programming"
+            };
+            HashSet<string> tags = new()
             {
                 "programming"
             };
@@ -132,6 +129,7 @@ namespace VideoConverter
             }
             if (video.Title.Contains("WPF"))
             {
+                tags.Add("C#");
                 tags.Add("WPF");
             }
             if (video.Title.Contains("XAML"))
@@ -140,79 +138,53 @@ namespace VideoConverter
             }
             if (video.Title.Contains("Material Design"))
             {
-                description += Environment.NewLine + "Material Design In XAML Project: https://github.com/MaterialDesignInXAML/MaterialDesignInXamlToolkit";
+                description += Environment.NewLine + Environment.NewLine + "Material Design In XAML Project: https://github.com/MaterialDesignInXAML/MaterialDesignInXamlToolkit";
                 tags.Add("material design");
+                playlists.Add("Material Design in Xaml");
+            }
+            if (video.Title.Contains("System.CommandLine"))
+            {
+                tags.Add("C#");
+                tags.Add("command line");
+                tags.Add("System.CommandLine");
+                playlists.Add("System.CommandLine");
+                description += Environment.NewLine + Environment.NewLine + "System.CommandLine: https://github.com/dotnet/command-line-api";
+            }
+            if (video.Title.Contains("terraform"))
+            {
+                tags.Add("terraform");
+                tags.Add("azure");
+                playlists.Add("Terraform");
+                playlists.Add("DevOps");
+                playlists.Add("Azure");
             }
 
-            description += Environment.NewLine + Environment.NewLine + $"Broadcasted live on Twitch -- Watch live at https://www.twitch.tv/kitokeboo";
+            description += Environment.NewLine + Environment.NewLine + $"Broadcasted live on Twitch -- Watch live at https://twitch.keboo.dev";
 
-            var videoUpload = new YouTubeVideo
+            var youtubeSection = config.GetSection("YouTube");
+
+            YouTubeBrowser browser = new(youtubeSection["Username"], youtubeSection["Password"], youtubeSection["RecoveryEmail"]);
+            await browser.UploadAsync(videoPath, video.Title, description, playlists, tags);
+            return "";
+        }
+
+        private static async Task DeleteFile(string file)
+        {
+            Exception? lastException = null;
+            for (int i = 0; i < 30; i++)
             {
-                Snippet = new VideoSnippet
+                try
                 {
-                    Title = video.Title,
-                    Description = description,
-                    Tags = tags.ToArray(),
-                    DefaultLanguage = "en-US",
-                    ChannelTitle = "Kevin Bost",
-                    //TODO: Should probably query this rather than hard coded....
-                    CategoryId = "28", // See https://developers.google.com/youtube/v3/docs/videoCategories/list,
-                    Localized = new()
-                    {
-                        Title = video.Title,
-                        Description = description
-                    }
-                },
-                Status = new VideoStatus
-                {
-                    License = "youtube",
-                    Embeddable = true,
-                    PrivacyStatus = "private", // or "unlisted" "private" or "public"
-                    //TODO: Just testing out the future publish setting, may not want to set this here.
-                    //PublishAt = DateTime.UtcNow + TimeSpan.FromDays(15),
-                },
-                RecordingDetails = new()
-                {
-                    LocationDescription = "Spokane",
-                    Location = new()
-                    {
-                        Latitude = 47.6587802,
-                        Longitude = -117.4260465,
-                        Altitude = 0
-                    },
-                    RecordingDate = DateTime.Parse(video.PublishedAt ?? video.CreatedAt),
+                    File.Delete(file);
+                    return;
                 }
-            };
-
-            using var fileStream = new FileStream(videoPath, FileMode.Open);
-            //Parts https://developers.google.com/youtube/v3/getting-started#part
-            var videosInsertRequest = service.Videos.Insert(videoUpload, "snippet,status,recordingDetails", fileStream, "video/*");
-            videosInsertRequest.ProgressChanged += ProgressChanged;
-            videosInsertRequest.ResponseReceived += ResponseReceived;
-
-            string videoId = "";
-            await videosInsertRequest.UploadAsync();
-            return videoId;
-
-            void ProgressChanged(IUploadProgress progress)
-            {
-                switch (progress.Status)
+                catch (Exception e)
                 {
-                    case UploadStatus.Uploading:
-                        //Console.WriteLine("{0} bytes sent.", progress.BytesSent);
-                        break;
-
-                    case UploadStatus.Failed:
-                        console.Out.WriteLine($"An error prevented the upload from completing.\n{progress.Exception}");
-                        break;
+                    lastException = e;
+                    await Task.Delay(TimeSpan.FromSeconds(1));
                 }
             }
-
-            void ResponseReceived(YouTubeVideo video)
-            {
-                videoId = video.Id;
-                console.Out.WriteLine($"Video id '{video.Id}' was successfully uploaded.");
-            }
+            throw lastException!;
         }
     }
 }
