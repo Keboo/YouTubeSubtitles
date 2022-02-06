555b789362c93f4608b31528d3ae8a1317c6f49f
Kevin Bost <kitokeboo@gmail.com>
2022-02-06 14:17:30 -0800

    Updating to .net6 C# 10
    Adding metadata extraction

---
+++ SubtitleConverter/VideoMetadata/Program.cs
@@ -0,0 +1,68 @@
+﻿using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using Microsoft.Azure.Cosmos.Table;
+using Microsoft.Extensions.Configuration;
+using StreamingTools.Azure;
+using StreamingTools.YouTube;
+
+DateTime OLDEST_DATE = new DateTime(2020, 12, 17);
+
+var configBuilder = new ConfigurationBuilder();
+configBuilder.AddEnvironmentVariables();
+configBuilder.AddUserSecrets(typeof(Program).Assembly);
+var config = configBuilder.Build();
+
+var azureSection = config.GetSection("Azure");
+string azureStorageAccountKey = azureSection["StorageAccountKey"] ?? throw new Exception(); //TODO
+var youtubeSection = config.GetSection("YouTube");
+string youTubeClientId = youtubeSection["ClientId"] ?? throw new Exception();
+string youTubeClientSecret = youtubeSection["ClientSecret"] ?? throw new Exception();
+
+CloudStorageAccount storageAccount = StorageAccount.Get(azureStorageAccountKey, config);
+var tableClient = storageAccount.CreateCloudTableClient();
+var youtubeSettingsTable = tableClient.GetTableReference("youtubesettings");
+
+YouTubeService youtubeService = await YouTubeFactory.GetServiceAsync(
+                new CloudTableDataStore(youtubeSettingsTable),
+                config,
+                youTubeClientId,
+                youTubeClientSecret,
+                YouTubeService.Scope.YoutubeForceSsl);
+
+var listVideosRequest = youtubeService.Search.List("snippet");
+listVideosRequest.Order = SearchResource.ListRequest.OrderEnum.Date;
+listVideosRequest.ForMine = true;
+listVideosRequest.MaxResults = 25;
+//listVideosRequest.PublishedAfter = DateTime.Now.AddYears(-1);
+listVideosRequest.Type = "video";
+
+using var writer = new StreamWriter("Videos.txt");
+do
+{
+    SearchListResponse? response;
+    response = await listVideosRequest.ExecuteAsync();
+    foreach (SearchResult item in response.Items)
+    {
+        if (item.Snippet.PublishedAt <= OLDEST_DATE)
+        {
+            Console.WriteLine($"Found item {item.Snippet.Title} published on {item.Snippet.PublishedAt}, ending");
+            return;
+        }
+        var videoDetails = youtubeService.Videos.List(new[] { "statistics", "snippet" });
+        videoDetails.Id = item.Id.VideoId;
+        var videoDetailsResponse = await videoDetails.ExecuteAsync();
+
+        Console.WriteLine($"Saving {item.Snippet.Title} ({item.Snippet.PublishedAt})");
+
+        await writer.WriteLineAsync($"Title: {item.Snippet.Title}");
+        await writer.WriteLineAsync($"Date: {item.Snippet.PublishedAt}");
+        await writer.WriteLineAsync($"Url: https://youtu.be/{item.Id.VideoId}");
+        await writer.WriteLineAsync($"Description: {item.Snippet.Description}");
+        await writer.WriteLineAsync($"Views: {videoDetailsResponse.Items[0].Statistics.ViewCount ?? 0}");
+        await writer.WriteLineAsync($"Tags: {string.Join(",", videoDetailsResponse.Items[0].Snippet.Tags ?? Array.Empty<string>())}");
+        await writer.WriteLineAsync("----");
+    }
+    await writer.FlushAsync();
+    listVideosRequest.PageToken = response.NextPageToken;
+}
+while (!string.IsNullOrWhiteSpace(listVideosRequest.PageToken));
